890e07c0cbbdd28892a632ab30e07d5839fa081a
Karizali <96954751+Karizali@users.noreply.github.com>
2022-11-24 18:46:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,285 @@
+// DICE
+// var a = Math.random();
+// var toss=Math.ceil(a*6);
+// console.log(toss);
+
+// Date and time
+// var months=['January',"Feburary","March","April","May","June","July","August","September","October","November","December"];
+// var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// var dt = new Date();
+// var a = dt.getMonth();
+// var d = dt.getDay();
+// var a = dt.getHours();
+// var a = dt.getFullYear();
+// var a = dt.getMinutes();
+// var a = dt.getSeconds();
+// var a = dt.getMilliseconds();
+
+// console.log(months[a]);
+// console.log(days[d]);
+
+
+// dt.setMonth(9);
+// dt.setDate(5);
+// dt.setFullYear(1582);
+// dt.setHours(0);
+// dt.setMinutes(0);
+// dt.setSeconds(1);
+// dt.setMilliseconds(0);
+
+//  console.log(dt);
+// Age
+// var dt = new Date();
+// var dt2 = new Date('06-21-2003');
+// var a=dt.getTime();
+// var b=dt2.getTime();
+// d=a-b;
+// g=d/(1000*60*60*24*365);
+
+// console.log(d);
+// console.log(g);
+// console.log(a);
+// console.log(b);
+
+// Function
+// function cal_of_dis(x,y){
+//     var Total_amount = x;
+//     var Discount = y;
+//     var After_discount = Total_amount-Discount;
+//     console.log(After_discount);
+// }
+
+// cal_of_dis(200,60);
+// cal_of_dis(599,30);
+// cal_of_dis(199,40);
+// cal_of_dis(999,20);
+
+
+// ///////////////////////////////////////////////////////////////////////
+// var dt1 = new Date();
+// var dt2 = new Date('02-17-2000');
+// console.log(dt2);
+
+// var dtTime1 = dt1.getTime();
+// var dtTime2 = dt2.getTime();
+// console.log(dtTime1);
+// console.log(dtTime2);
+
+// var diff = dtTime1 - dtTime2;
+// var age  = diff/(1000*365*24*60*60);
+// console.log(parseInt(age));
+// console.log(age);
+// console.log(Math.floor(age));
+ 
+
+
+// function calculateDiscount(a,b){
+//     var discount = b;
+//     var amount = a;
+
+//     var total = amount - discount;
+
+//     console.log(total);
+// }
+
+// calculateDiscount(100,20);
+// calculateDiscount(200,30);
+// calculateDiscount(300,40);
+// calculateDiscount(400,50);
+
+// function calculatePercentageDiscount(a,b){
+//     var discountPrice = (a*b)/100;
+//     var amount = a;
+
+//     var total = amount - discountPrice;
+
+//     console.log(total);
+// }
+
+
+// calculatePercentageDiscount(100,10);
+// calculatePercentageDiscount(200,15);
+
+
+// var a="ABC";
+
+// function xyz(){
+//     console.log(a);
+// }
+
+// xyz();
+
+// var a="ABC";
+
+// function xyz(){
+//     a="123";
+// }
+// xyz();
+// console.log(a);
+ 
+
+// function calculateDiscount(a,b){
+//         var discount = b;
+//         var amount = a;
+    
+//         var total = amount - discount;
+    
+//         return total;
+//     }
+
+//     var item = calculateDiscount(100,10)
+//     console.log(item );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Local variable
+// function students(){
+    //     total = 10+"Students"
+        
+    // }
+    // console.log(total);
+    
+
+
+
+//Global variable
+// var fruit="apple"
+// function fav(){
+
+//     console.log(fruit);
+// }
+// fav();
+
+
+
+
+
+// var count=0;
+// function addcount(){
+//     count=1;
+// }
+// addcount();
+// console.log(count);
+
+
+
+// function abc(){
+//     var a=10;
+//     var b=34;
+//     var e=3
+//     return b;
+// }
+// c=abc();
+
+// console.log(c);
+
+
+//SWITCH CASE
+// var Days="tue";
+// switch(Days){
+//     case "mon":
+//         console.log("Working day 1");
+//         break;
+//     case "tue":
+//         console.log("Working day 2");
+//         break;
+//     case "wed":
+//         console.log("Working day 3");
+//         break;
+//     case "thur":
+//         console.log("Working day 4");
+//         break;
+//     case "fri":
+//         console.log("Working day 5");
+//         break;
+//     case "sat":
+//         console.log("Holiday 1");
+//         break;
+//     case "sun":
+//         console.log("Holiday 2");
+//         break;
+//     default:
+//         console.log("No Day found");
+
+// }
+
+
+
+// While Loop
+
+// var i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// var i=0;
+// while(i<10){
+//     i++
+//     console.log(i);
+// }
+
+// DO While
+
+// var i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+
+
+
+// var i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i>10);
+
+function addition(){
+    var a=10;
+    var b=5;
+    var c=a+b;
+    console.log(c)
+}
+
+function abc(){
+    var r=12;
+    console.log(r);
+}
+function xyz(){
+    
+    var f=124421;
+    console.log(f);
+}
+
+
+
+
+
+
+
+
+
+
+
